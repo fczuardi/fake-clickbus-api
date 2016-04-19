2c2f2d47fe42efec5bd0d1c6ecddca859d35c74e
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-19 14:22:59 -0300

    external shebang with harmony flags for nodejs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!./shebang.sh
+#!harmony
 
 const request = require('request');
 const fakeClickBus = require('./lib');
@@ -6,23 +6,25 @@ const fakeClickBus = require('./lib');
 const CLICKBUS_URL = process.env.CLICKBUS_URL || 'https://api-evaluation.clickbus.com.br/api/v1';
 fakeClickBus(CLICKBUS_URL);
 
-// request(CLICKBUS_URL + '/places', function (error, response, body) {
-//     console.log(response.body)
-// });
+request(`${CLICKBUS_URL}/places`, (error, response, body) => {
+    const firstPlace = JSON.parse(body).items[0];
+    console.log(firstPlace);
+});
 
-// request(CLICKBUS_URL + '/trips', function (error, response, body) {
-//     console.log(response.body)
-// });
+request(`${CLICKBUS_URL}/trips`, (error, response, body) => {
+    console.log(body);
+});
 
-// request(CLICKBUS_URL + '/trips?from=sao-paulo-tiete-sp&departure=2015-01-01',
-//     function (error, response, body) {
-//         console.log(response.body)
-//     }
-// );
+request(`${CLICKBUS_URL}/trips?from=sao-paulo-tiete-sp&departure=2015-01-01`,
+    (error, response, body) => {
+        console.log(body);
+    }
+);
 
 const today = new Date().toISOString().slice(0, 10);
 request(`${CLICKBUS_URL}/trips?from=sao-paulo-tiete-sp&departure=${today}`,
     (error, response, body) => {
-        console.log(body);
+        const firstTrip = JSON.parse(body).items[0];
+        console.log(firstTrip);
     }
 );
