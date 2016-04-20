55112c2120446d69a68f699671cdb9ddfbf5eb21
Fabricio C Zuardi <fabricio@fabricio.org>
2016-04-20 15:13:24 -0300

    simplified duplicated code

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const fakeClickBus = require('./lib');
 const CLICKBUS_URL = process.env.CLICKBUS_URL || 'https://api-evaluation.clickbus.com.br/api/v1';
 fakeClickBus(CLICKBUS_URL);
 
+const getFirstItem = (body) => (JSON.parse(body).items[0]);
+
 request(`${CLICKBUS_URL}/places`, (error, response, body) => {
-    const firstPlace = JSON.parse(body).items[0];
-    console.log(firstPlace);
+    console.log(getFirstItem(body));
 });
 
 request(`${CLICKBUS_URL}/trips`, (error, response, body) => {
@@ -24,7 +25,6 @@ request(`${CLICKBUS_URL}/trips?from=sao-paulo-tiete-sp&departure=2015-01-01`,
 const today = new Date().toISOString().slice(0, 10);
 request(`${CLICKBUS_URL}/trips?from=sao-paulo-tiete-sp&departure=${today}`,
     (error, response, body) => {
-        const firstTrip = JSON.parse(body).items[0];
-        console.log(firstTrip);
+        console.log(getFirstItem(body));
     }
 );
